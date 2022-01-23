0f82ce9974a6d0cbbed0407ade0ef66dc87a2ba1
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-23 11:38:25 -0300

    Revisando tipos de dados em javaScript

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-document.querySelector()
+//Variáveis
+//string
+//var nome = 'Johnny' (var descontinuado)
 
+//number
+//var idade = '35'
+
+//let nome = 'Johnny'
+/*
+const nome = 'Thiago'
+nome = 'Vai dar erro'
+console.log(nome)
+*/
+
+//Tipos de dados
+/*
+string
+number
+boolean
+array
+object
+undefined
+null
+*/
+
+
+//string
+let nome = 'Johnny' 
+console.log(nome)
+
+//string - concatenada
+let sobrenome = 'Marlon'
+console.log(nome + ' ' + sobrenome)
+
+//string literal
+console.log(`${nome} ${sobrenome}`)
