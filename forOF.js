58d08cca6d242f5c6f548bbba358a1f718b14eec
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-24 20:55:42 -0300

    Revisando for e for of

--- forOF.js
+++ forOF.js
@@ -16,6 +16,25 @@ let clientes = [
   }
 ]
 
+let htmlClientes = ''
+
 for (let cliente of clientes) {
-  console.log(cliente)
+  let listaLinguagens = ''
+
+  for (linguagem of cliente.linguagens) {
+    listaLinguagens += `<li>${linguagem}</li>`
+  }
+
+  htmlClientes += `
+  <li>
+    <b>Nome:</b> ${cliente.nome}<br>
+    <b>Idade:</b> ${cliente.idade}<br>
+    <b>Linguagens:</b><br>
+    <ul>
+      ${listaLinguagens}
+    </ul>
+  </li>  
+  `
 }
+
+document.querySelector('#listaClientes').innerHTML = htmlClientes
