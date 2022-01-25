f1b80f224d5cee38cb6e7100d975decb291d3692
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-24 21:22:30 -0300

    Revisando  functions

--- functions.js
+++ functions.js
@@ -1,5 +1,21 @@
-function escreverNoConsole(mensagem) {
-  console.log(`${mensagem}`)
+/*
+function escreverNoConsole(mensagem = '', nome = '') {
+  console.log(`Olá ${nome}, ${mensagem}`)
 }
 
-escreverNoConsole('Chupa meu frango!')
+escreverNoConsole('Seja muito bem vindo(a)', 'Thiago')
+*/
+
+function soma(num1, num2) {
+  return num1 + num2
+}
+
+console.log(soma(10, 5))
+
+function milhasEmKm(milhas) {
+  let km = milhas * 1.6
+  return km
+}
+
+let resultado = milhasEmKm(150)
+document.write(`O resultado é: ${resultado} km`)
