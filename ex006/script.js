a77826639048bd6a35b60886e9e51f6a4d157dca
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-01-25 22:22:58 -0300

    Exercícios dos conceitos revisados em javaScript (for criando div´s dinamicamente)

--- ex006/script.js
+++ ex006/script.js
@@ -10,23 +10,40 @@ let listaAtores = [
     personagem: 'Darth Vader',
     filme: 'Star Wars Episódios IV-VI'
   },
-
+  
   {
     nome: 'Bruce Wayne',
     personagem: 'Batman',
     filme: 'Batman - O Início'
+  },
+
+  {
+    nome: 'Johnny Deep',
+    personagem: 'Capitão Sparrow',
+    filme: 'Piratas do caribe'
+  },
+
+  {
+    nome: 'Wagner Moura',
+    personagem: 'Capitão Nascimento',
+    filme: 'Tropa de elite'
+  },
+
+  {
+    nome: 'Jack Black',
+    personagem: 'JB',
+    filme: 'Tenacious D uma dupla infernal'
   }
 ]
 
 for (let c = 0; c < listaAtores.length; c++) {
   let ator = listaAtores[c]
-  document.querySelector('.container')
-    .innerHTML += `<div class="box box${c + 1}">
+  document.querySelector('.container').innerHTML += `<div class="box box${c +
+    1}">
                     <h1>${ator.nome}</h1>
                     <p>
                         Interpreta o personagem ${ator.personagem} 
                         no filme ${ator.filme}
                     </p>
                  </div>`
-                
 }
